bfc77d1505ba8b11e792bf88d7d32859c52e4bf5
PauloGuerra2019 <46049000+PauloGuerra2019@users.noreply.github.com>
2019-01-16 15:28:34 -0200

    Add files via upload

---
+++ meuscript.js
@@ -0,0 +1,37 @@
+var nomeCanal ='thisforcin2';
+$(document).ready(function(){
+   $.get("https://www.googleapis.com/youtube/v3/channels",{
+       part:'contentDetails',
+       forUsername:nomeCanal,
+       key:'AIzaSyAEp7TcVrsmkT1fEyVH9q1OUGGFqyfIP4s'
+            },
+         function(data){
+            upload_id = data.items[0].contentDetails.relatedPlaylists.uploads;
+            pegarVideos(upload_id);        
+   
+            }
+        
+        ) 
+    
+    function pegarVideos(id){
+       $.get("https://www.googleapis.com/youtube/v3/playlistItems",{
+           part:'snippet',
+           maxResults: 12,
+           playlistId: id,
+           key:'AIzaSyAEp7TcVrsmkT1fEyVH9q1OUGGFqyfIP4s'
+       },
+             function(data){
+               var imagem;
+               var arquivo;   
+           $.each(data.items, function(i, item){
+              imagem = item.snippet.thumbnails.medium.url;
+              titulo = item.snippet.title; 
+              descricao = item.snippet.publishedAt;
+              videoId =item.snippet.resourceId.videoId;   
+              arquivo = '<li class="principal"><div class="foto"><img src ="' + imagem + '"/><div class="legenda"><a href="http://www.youtube.com/watch?v='+videoId+'" class="nyroModal"></a><h5>'+ titulo +'</h5><p>'+ descricao +'</p></div></div></li>';
+               $('div#janela ul').append(arquivo);
+           });
+        }
+      )
+    }
+});
